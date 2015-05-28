1d505ccd7c59aba337238120c52fc8759c5dc37a
Cubiccl <ccl.clement@hotmail.fr>
2015-05-28 12:54:56 +0200

    RPGProject with the game

--- src/main/RPGProject.java
+++ src/main/RPGProject.java
@@ -1,11 +1,17 @@
 package main;
 
+import main.game.Game;
+
 public class RPGProject
 {
+	/** The main game */
+	private static Game game;
 
+	/** Called when starting the program. */
 	public static void main(String[] args)
 	{
+		game = new Game();
+		game.start();
 	}
 
 }
-
