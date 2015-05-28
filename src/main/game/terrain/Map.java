69323f4056348fb043cfa85a60e4f5dd6eac94e0
Darkxell <darkxell.mc@gmail.com>
2015-05-28 15:57:26 +0200

    Updated map

--- src/main/game/terrain/Map.java
+++ src/main/game/terrain/Map.java
@@ -5,11 +5,16 @@ public class Map {
 	/** The tiles of the map */
 	private short[][] tiles;
 
+	/**The default size of the map*/
 	private final short DEFAULT_SIZE = 255;
 
+	/**The actual size of the map*/
+	private short actual_size;
+	
 	/** Default constructor of an empty map */
 	public Map() {
 		this.tiles = new short[DEFAULT_SIZE][DEFAULT_SIZE];
+		this.actual_size = DEFAULT_SIZE;
 	}
 
 	/**
@@ -25,7 +30,18 @@ public class Map {
 				this.tiles[i][j] = tile;
 			}
 		}
-
+		this.actual_size = DEFAULT_SIZE;
 	}
+	
+	/**Gets the tile id at the specified value*/
+	public short getTileAt(short x, short y){
+		return this.tiles[x][y];
+	}
+	
+	/**Gets the size of the map (it's a square)*/
+	public short getSize(){
+		return this.actual_size;
+	}
+	
 
 }
